81e41988deef23e35fbef577f70fdf59285e6692
kvnxiao <xlzqwerty1@gmail.com>
2020-03-12 16:11:04 -0400

    Update clock and qotd styling

---
+++ src/clock/clock.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  #--ID--
+    p {{ time }}
+</template>
+
+<script>
+const moment = require("moment")
+
+export default {
+  data() {
+    return {
+      time: "",
+      config: null,
+    }
+  },
+
+  methods: {
+    updateTime() {
+      if (this.config && this.config.format) {
+        this.time = moment().format(this.config.format)
+      } else {
+        this.time = moment().format("hh : mm : ss A")
+      }
+    },
+  },
+
+  mounted() {
+    this.updateTime()
+    setInterval(() => {
+      this.updateTime()
+    }, 1000)
+  },
+}
+</script>
+
+<style lang="sass">
+#--ID--
+  font-size: 80px
+  font-family: "Roboto", sans-serif
+  color: #FFFFFF
+</style>
--- src/quoteoftheday/quoteoftheday.vue
+++ src/quoteoftheday/quoteoftheday.vue
@@ -1,36 +1,12 @@
 <template lang="pug">
-  #--ID--
-    h4 Quote of the Day:
-    <div style="width=100">
-    h9 "{{ quoteText }}"
-    h9  - {{quoteAuthor}}
-    </div>
+#--ID--
+  h4.title Quote of the Day:
+  h4.quote "{{ quoteText }}"
+  h4.author - {{quoteAuthor}}
 </template>
 
 <script>
-async function makeRequest(method, url) {
-  return new Promise(function(resolve, reject) {
-    let xhr = new XMLHttpRequest()
-    xhr.open(method, url)
-    xhr.onload = function() {
-      if (this.status >= 200 && this.status < 300) {
-        resolve(xhr.response)
-      } else {
-        reject({
-          status: this.status,
-          statusText: xhr.statusText,
-        })
-      }
-    }
-    xhr.onerror = function() {
-      reject({
-        status: this.status,
-        statusText: xhr.statusText,
-      })
-    }
-    xhr.send()
-  })
-}
+const axios = require("axios").default
 
 export default {
   data() {
@@ -39,49 +15,39 @@ export default {
       quoteAuthor: "",
     }
   },
+
   methods: {
-    async makeRequest(method, url) {
-      return new Promise(function(resolve, reject) {
-        let xhr = new XMLHttpRequest()
-        xhr.open(method, url)
-        xhr.onload = function() {
-          if (this.status >= 200 && this.status < 300) {
-            resolve(xhr.response)
-          } else {
-            reject({
-              status: this.status,
-              statusText: xhr.statusText,
-            })
-          }
-        }
-        xhr.onerror = function() {
-          reject({
-            status: this.status,
-            statusText: xhr.statusText,
-          })
-        }
-        xhr.send()
+    update() {
+      axios.get("http://quotes.rest/qod.json").then(resp => {
+        console.log()
+        const quote = resp.data.contents.quotes[0].quote
+        const author = resp.data.contents.quotes[0].author
+        this.quoteText = quote
+        this.quoteAuthor = author
       })
     },
   },
-  async mounted() {
-    const url = "http://quotes.rest/qod.json"
-    const result = await makeRequest("GET", url)
-    const data = JSON.parse(result)
-    const quote = data.contents.quotes[0].quote
-    const author = data.contents.quotes[0].author
-    this.quoteText = quote
-    this.quoteAuthor = author
+
+  mounted() {
+    // Update every 3 hours
+    this.update()
+    setInterval(() => {
+      this.update()
+    }, 3600 * 3)
   },
 }
 </script>
 
 <style lang="sass">
-@import url("https://fonts.googleapis.com/css?family=Roboto")
-
 #--ID--
+  padding: 2rem
   font-size: 30px
-  font-family: "Times New Roman"
-  color: #daf6ff
-  text-shadow: 0 0 20px rgba(10, 175, 230, 1),  0 0 20px rgba(10, 175, 230, 0)
+  font-family: "Roboto", sans-serif
+  color: #ffffff
+
+  .title
+    margin-bottom: 1rem
+
+  .author
+    text-align: right
 </style>
